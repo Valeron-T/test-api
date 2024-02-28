136e40cdeb9a3550699bcf42cd3d3689167e1961
Valeron <valerontoscano@gmail.com>
2024-02-28 19:42:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    # Your processing logic
+    await asyncio.sleep(2)
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
