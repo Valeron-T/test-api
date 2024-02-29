66823ffdd866315f499cdd20bd7e5c8c59daebcb
Valeron <valerontoscano@gmail.com>
2024-02-29 19:36:20 +0530

    Added own API as intermediary

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import asyncio
 from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+
+class CardInfo(BaseModel):
+    name: str
+    number: int
+    cvv: int
+    exp: str
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    # Your processing logic
     # await asyncio.sleep(2)
-    return {"message": "Hello World"}
+    return {"message": "API is running"}
+
+
+@app.post("/checkout")
+async def checkout(card: CardInfo):
+    # Some processing logic - Check inventory, Validate address etc.
+    # Assume everything is valid and pass info to payment gateway
+    furl = f"https://mockservicecustomapi340180.mock.blazemeter.com/api/validate?name={card.name.replace(' ', '%20')}&cvv={card.cvv}&exp={card.exp}&cardnumber={card.number}"
+    response = requests.get(furl)
+    print(furl)
+    return {"status": response.json()}
 
 
 @app.get("/hello/{name}")
